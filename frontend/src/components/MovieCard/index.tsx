07473e098f4c19dd6819d004dee2a71025fc3cdd
Benior <benior.santos@gmail.com>
2022-05-15 13:07:32 -0300

    Save, useNavigate

--- frontend/src/components/MovieCard/index.tsx
+++ frontend/src/components/MovieCard/index.tsx
@@ -3,25 +3,24 @@ import { Link } from "react-router-dom";
 import { Movie } from "types/movie";
 
 type Props = {
-    movie: Movie
+    movie: Movie;
 }
 
-function MovieCard({ movie } : Props) {
-    
-
+function MovieCard( { movie } : Props) {
 
     return (
         <div>
             <img className="dsmovie-movie-card-image" src={movie.image} alt={movie.title} />
             <div className="dsmovie-card-bottom-container">
                 <h3>{movie.title}</h3>
-                <MovieScore count={movie.count} score={movie.score}/>
-                <Link to={'form/{movie.id}'}>
+                <MovieScore count={movie.count} score={movie.score} />
+
+                <Link to={`/form/${movie.id}`}>
                     <div className="btn btn-primary dsmovie-btn">Avaliar</div>
                 </Link>
+
             </div>
         </div>
-
     );
 }
 
